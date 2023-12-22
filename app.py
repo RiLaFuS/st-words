3bc372174ce3199800425c3fb515fb0d41510332
RiLaFuS <ce26415@gmail.com>
2023-12-22 23:37:11 +0900

    1st commmit

--- app.py
+++ app.py
@@ -21,14 +21,15 @@ set_bg_color()
 image = Image.open('image.png')
 st.image(image, use_column_width=True)
 
-st.write('提供されたcsvファイルに含まれる単語を解析し、出現数上位の10単語を表示します。')
-st.write('顧客アンケート、社内情報など、文章の解析にご活用ください。')
+st.write('## SAKURAとは')
+st.write('お客様の声を簡単に解析するSAKURAは、テキストファイル（CSV 形式）をブラウザからアップロードすることで、AI 機能を用いた自動解析が可能なサービスです。口コミやアンケート、問合せなどから注目すべきキーワードを探します。')
+st.write('顧客ニーズの調査や自社の課題解決、サービス改善などにお役立てください。')
 
 # Janomeの設定
 janome_tokenizer = Tokenizer()
 
-uploaded_file = st.file_uploader('CSVを選択', type='csv')
-select_pos = st.sidebar.multiselect('品詞選択', ['名詞', '固有名詞', '動詞', '形容詞', '副詞', '助詞'], ['名詞'])
+uploaded_file = st.file_uploader('↓ 今すぐ解析 ↓ （csvファイルを読み込んでください）', type='csv')
+select_pos = st.sidebar.multiselect('↓ 解析したい品詞を選択していただけます。', ['名詞', '固有名詞', '動詞', '形容詞', '副詞', '助詞'], ['名詞'])
 
 def janome_parse(text):
     words = []
@@ -40,7 +41,7 @@ def janome_parse(text):
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    tg_col = st.selectbox('対象とする列名の選択', df.columns)
+    tg_col = st.selectbox('↓ csvファイルが読み込めました。次に、解析したい列名を選択してください。 ↓', df.columns)
     if tg_col is not None:
         df = df.dropna()  # 欠損値がある行を削除
         input_text = df[tg_col]
@@ -52,12 +53,12 @@ if uploaded_file is not None:
             words_df = words_df.groupby('Word', as_index=False).size()
             words_df.sort_values('size', ascending=False, inplace=True)
 
-            st.write('読み込んだcsvファイルの内容')
+            st.write('アップロードされたcsvファイルの内容は以下です。')
             st.dataframe(df)
             
             import matplotlib.pyplot as plt
             import japanize_matplotlib
-            data = words_df.head(10)
+            data = words_df.head(20)
             # グラフを作成
             fig, ax = plt.subplots()
             ax.bar(data['Word'], data['size'])
@@ -67,7 +68,9 @@ if uploaded_file is not None:
             plt.ylabel('出現回数', fontsize=14)  # Y軸のラベルと文字サイズを設定
             plt.xticks(rotation=45, fontsize=12)  # X軸のラベルを回転させて表示
             plt.yticks(fontsize=12)  # Y軸のラベルの文字サイズを設定
-            plt.title('出現回数の多い上位10単語', fontsize=16)  # グラフのタイトルと文字サイズを設定
+            plt.title('出現頻度が高い20個の「キーワード」', fontsize=16)  # グラフのタイトルと文字サイズを設定
 
             # StreamlitにMatplotlibのグラフを表示
             st.pyplot(fig)
+            st.write('解析結果の表は以下の通りです。')
+            st.dataframe(data)
